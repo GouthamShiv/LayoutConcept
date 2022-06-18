8d1c8a8b3721e97882ee480f1eb5e9f02c1396a5
Goutham S <gouthamshine@gmail.com>
2022-06-18 11:10:11 +0530

    Initial Commit

---
+++ LayoutConcept/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutConcept
+//
+//  Created by Goutham on 18/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutConcept/LayoutConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutConceptApp.swift
+//  LayoutConcept
+//
+//  Created by Goutham on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
